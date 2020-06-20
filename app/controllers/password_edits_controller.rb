1243c169feb39995c110f843c4e6598581d206b0
Naoya <naoya.ozawa.y@gmail.com>
2020-06-20 08:47:07 +0000

    Implement profile and password change function

--- app/controllers/password_edits_controller.rb
+++ app/controllers/password_edits_controller.rb
@@ -5,6 +5,40 @@ class PasswordEditsController < ApplicationController
   end
   
   def update
+    @user = User.find(params[:id])
+    #現在のパスワードが一致しない場合はeditをrenderして終了
+    unless @user.authenticated?(:password, params[:user][:current_password])
+      flash.now[:danger] = "Incorrect password"
+      render 'edit'
+      return
+    end
     
+    if @user.update_attributes(user_params)
+        flash[:success] = "Password changed"
+        redirect_to @user
+    else
+      render 'edit'
+    end
   end
+  
+  # def update
+  #   @user = User.find(params[:id])
+  #   if @user.authenticated?(:password, params[:user][:current_password])
+  #     if @user.update_attributes(user_params)
+  #       flash[:success] = "Password changed"
+  #       redirect_to @user
+  #     else
+  #       render 'edit'
+  #     end
+  #   else
+  #     flash.now[:danger] = "Incorrect password"
+  #     render 'edit'
+  #   end
+  # end
+  
+  private
+  
+    def user_params
+      params.require(:user).permit(:password, :password_confirmation)
+    end
 end
