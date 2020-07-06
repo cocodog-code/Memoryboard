aa93ae572c3a2f51908257cfb93c5859ace20d59
Naoya <naoya.ozawa.y@gmail.com>
2020-07-06 03:07:20 +0000

    Fix facebook login

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,14 +14,14 @@ class User < ApplicationRecord
   attr_accessor :remember_token
   before_save { self.email = email.downcase }
   validates :full_name, presence: true, unless: :uid?, length: { maximum: 50 }
-  validates :user_name, presence: true, unless: :uid?, length: { maximum: 50 }
+  validates :user_name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGAX = /\A[\w+\-.]+@[a-z\d\-.]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
-  validates :email, presence: true, unless: :uid?, length: { maximum: 255 },
+  validates :email, presence: true, length: { maximum: 255 },
                 format: { with: VALID_EMAIL_REGAX },
                 uniqueness: true
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 },
-  allow_nil: true, unless: :uid?
+  allow_nil: true, on: :facebook_login
   
   # 渡された文字列のハッシュ値を返す
   def User.digest(string)
