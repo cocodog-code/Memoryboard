0e843e760d5840d4d3113573a3ff9d30655b907e
Naoya <naoya.ozawa.y@gmail.com>
2020-07-06 02:42:05 +0000

    Fix facebook login

--- app/models/user.rb
+++ app/models/user.rb
@@ -87,7 +87,7 @@ class User < ApplicationRecord
      user = User.new
   end
   user.uid   = auth.uid
-  user.username  = auth.info.name
+  user.user_name  = auth.info.name
   user.email = auth.info.email
   user.oauth_token = auth.credentials.token
   user.oauth_expires_at = Time.at(auth.credentials.expires_at)
