be33940fd7c828f29e4be1453efd67821279e67a
Naoya <naoya.ozawa.y@gmail.com>
2020-07-07 00:38:38 +0000

    Fix facebook login

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,7 @@ Rails.application.routes.draw do
   get    '/login',  to: 'sessions#new'
   post   '/login',  to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
-  get '/auth/:provider/callback', to: 'users#facebook_login', as: :auth_callback
-  get '/auth/failure', to: 'users#auth_failure', as: :auth_failure
+  get 'auth/:provider/callback', to: 'sessions#create'
   resources :users do
     member do
       get :following, :followers
