5e790f57cd2a3c44d7d68cba398fe10a4202325e
Naoya <naoya.ozawa.y@gmail.com>
2020-07-09 07:27:37 +0000

    Fix facebook

--- app/models/user.rb
+++ app/models/user.rb
@@ -93,7 +93,6 @@ class User < ApplicationRecord
     provider = auth[:provider]
     uid = auth[:uid]
     name = auth[:info][:name]
-  
   　#データベースにユーザが存在するならユーザ取得し、存在しないなら新しいユーザを作成する
     #ユーザはSNSで登録情報を変更するかもしれので、毎回データベースの情報も更新する
     self.find_or_create_by(provider: provider, uid: uid) do |user|
