e2021ddea2ad7d36fb0aa17ebab7fd737cb1f742
Naoya <naoya.ozawa.y@gmail.com>
2020-07-14 04:41:50 +0000

    Add like button to user-show

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -4,7 +4,7 @@ class FavoritesController < ApplicationController
   def create
     @micropost = Micropost.find(params[:micropost_id])
     @micropost.like(current_user)
-    # micropost = Micropost.find(params[:micropost_id])
+    micropost = Micropost.find(params[:micropost_id])
     micropost.create_notification_like!(current_user)
     respond_to do |format|
         format.html { redirect_to request.referrer || root_url }
